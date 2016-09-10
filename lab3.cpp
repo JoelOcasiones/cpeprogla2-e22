3b00b7356bf2e588331aa0f11d75e3f9a09a097a
JOEL CHRISTIAN OCASIONES <JAOCASIONES@fit.edu.ph>
2016-09-10 11:25:40 +0800

    Adss custom strcpy

--- lab3.cpp
+++ lab3.cpp
@@ -1,15 +1,38 @@
 #include<iostream>
 #include<cstring>
+
 using namespace std;
 
-char* yo(char* str){
-  //return strlen(str);
-  return str;
+int myStrCmp(char* str1, char*str2){
+	switch(strcmp(str1, str2)){
+		case -1:
+			cout<<"Negative";
+			break;
+		case 1:
+			cout<<"Positive";
+			break;
+		case 0:
+			cout<<"Equal";
+			break;		
+	}
+	
+}
+
+void myStrCpy(char* str1, char* str2){
+	strcpy(str1, str2);
+	
 }
 
 int main(){
-  char str1[100];
-  char str2[100];
-  strcpy(str1,"Hello there");
-  cout << yo(str1);
+	char str1[100]="Hello";
+	char str2[100]="hello";
+	myStrCmp(str1, str2);
+	myStrCpy(str1, str2);
+	cout<<str1;
 }
+
+
+
+
+
+
