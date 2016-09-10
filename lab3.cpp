efbb94bc469308b89242c11a25338d57aaeccf90
JoelOcasiones <jocasiones@yahoo.com>
2016-09-10 11:39:04 +0800

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -21,6 +21,11 @@ int myStrCmp(char* str1, char*str2){
 void myStrCpy(char* str1, char* str2){
 	strcpy(str1, str2);
 	
+	
+}
+
+void myStrCat(char* str1, char* str2){
+	strcat(str1, str2);
 }
 
 int main(){
@@ -28,9 +33,9 @@ int main(){
 	char str2[100]="hello";
 	myStrCmp(str1, str2);
 	myStrCpy(str1, str2);
+	myStrCat(str1, str2);
 	cout<<str1;
 }
-
 
 
 
